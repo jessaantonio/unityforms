ad79d3a0bbc601406939fb5b5d46afe0283b3489
dukecg <dukecg@6659471e-fef1-11de-89cc-8daf83ecd611>
2010-01-21 10:11:57 +0000

    Added ToString overrides to UnityFormsDesigner posted on Friday 15th - probably superseded!

---
+++ UnityForms/Location.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace UnityForms
+{
+    // Added ToString override to file from Friday 15th Jan
+
+    public struct Location
+    {
+        public float X;
+        public float Y;
+
+        public static Location Zero
+        {
+            get
+            {
+                return new Location(0, 0);
+            }
+        }
+
+        public Location (Rect rect)
+        {
+            this.X = rect.x;
+            this.Y = rect.y;
+        }
+
+        public Location(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("(X: {0:F1}, Y: {1:F1})", this.X, this.Y);
+        }
+    }
+}
