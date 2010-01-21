c0635636a4f166f3b98a00ad513f549c5268fdd4
dukecg <dukecg@6659471e-fef1-11de-89cc-8daf83ecd611>
2010-01-21 10:11:57 +0000

    Added ToString overrides to UnityFormsDesigner posted on Friday 15th - probably superseded!

---
+++ trunk/UnityForms/Size.cs
@@ -0,0 +1,29 @@
+﻿namespace UnityForms
+{
+    // Added ToString override to file from Friday 15th Jan
+
+    public struct Size
+    {
+        public float Width;
+        public float Height;
+
+        public static Size Zero
+        {
+            get
+            {
+                return new Size(0, 0);
+            }
+        }
+
+        public Size (float width, float height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("(W: {0:F1}, H: {1:F1})", this.Width, this.Height);
+        }
+    }
+}
